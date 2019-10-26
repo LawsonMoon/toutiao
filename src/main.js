9ff7268d9ed33226b4fed0acc22ec4d05c6d5b36
LawsonMoon <1535355374@qq.com>
2019-10-26 10:36:03 +0800

    登陆模块结构

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
+// 1. 导入某一个目录，默认会去找索引文件（index.js index.vue index.json） 基于webpack
+// 2. @ 别名  指定的是 /src 路径  一个绝对路径。 基于webpack
+// import router from './router/index.js'
+import router from '@/router'
+
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,14 @@
+import VueRouter from 'vue-router'
+import Vue from 'vue'
+import login from '../views/login/index.vue'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes: [{
+    path: '/login',
+    component: login
+  }]
+})
+
+export default router
